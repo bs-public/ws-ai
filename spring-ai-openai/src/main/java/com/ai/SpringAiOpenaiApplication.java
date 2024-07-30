9cf484a3ec87d54b759c7a16faf62bc1c33465b3
bs <26665808+bs-public@users.noreply.github.com>
2024-07-30 21:17:59 +1000

    Code formatted

--- spring-ai-openai/src/main/java/com/ai/SpringAiOpenaiApplication.java
+++ spring-ai-openai/src/main/java/com/ai/SpringAiOpenaiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAiOpenaiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAiOpenaiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAiOpenaiApplication.class, args);
+  }
 
 }
